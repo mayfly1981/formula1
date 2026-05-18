075413435a3796d89ed36bd26cfb514ae591def6
Ksendza06 <ksendzavuu@gmail.com>
2026-05-18 14:25:54 +0200

    Add driver details

---
+++ src/DriversDetails.jsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import Loader from "../../smart/src/components/Loader";
+
+export default function DriverDetails() {
+    const [driverDetails, setDriverDetails] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const params = useParams();
+
+    useEffect(() => {
+        getDriverDetails();
+    }, []);
+
+    const getDriverDetails = async () => {
+        console.log("params", params);
+        const url = "https://api.jolpi.ca/ergast/f1/2013/drivers/alonso/driverStandings.json";
+
+        const response = await axios.get(url);
+        console.log("response", response);
+        // setDriverDetails(response.data);
+        setIsLoading(false);
+        console.log("getDriversDetails");
+    };
+
+    if (isLoading) {
+        return <Loader />;
+    };
+
+    return (
+        <>
+            <div>
+                <h2>Comment details</h2>
+                <p>Id: {driverDetails.driverId}</p>
+                <p>Id: {driverDetails.permanentNumber}</p>
+                <p>Id: {driverDetails.code}</p>
+                <p>Id: {driverDetails.givenName}</p>
+                <p>Id: {driverDetails.familyName}</p>
+                <p>Id: {driverDetails.dateOfBirth}</p>
+                <p>Id: {driverDetails.nationality}</p>
+                {/* <p>Name: {DriverDetails.name}</p>
+                        <p>Email: {DriverDetails.email}</p>
+                        <p>Body: {DriverDetails.body}</p> */}
+            </div>
+
+            {/* <p>Id: {driverDetails.id}</p>
+            <p>Name: {driverDetails.name}</p>
+            <p>Email: {driverDetails.email}</p>
+            <p>Body: {driverDetails.body}</p> */}
+        </>
+    );
+}
