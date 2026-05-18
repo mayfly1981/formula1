9cd2e0407ebd47e850579695d5594c158550b843
mayfly1981 <mayfly1981@gmail.com>
2026-05-18 11:47:44 +0200

    rute-kostur

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router";
+import Home from "./components/Drivers";
+import About from "./components/Teams";
+import Contact from "./components/Races";
+
 export default function formula1() {
   return (
-    <>
-      FORMULA 1
-    </>
-  )
+
+    <BrowserRouter>
+      {/*Navigacija*/}
+      <nav className="top-navigation">
+        <ul>
+          <li><Link to="/">Drivers</Link></li>
+          <li><Link to="/teams">Teams</Link></li>
+          <li><Link to="/races">Races</Link></li>
+        </ul>
+      </nav>
+
+      {/* Rute */}
+      <div style={{ backgroundColor: "#ccc" }}>
+        <Routes>
+          <Route path="/" element={<Drivers />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/races" element={<Races />} />
+        </Routes>
+      </div>
+
+    </BrowserRouter >
+
+
+  );
 }
