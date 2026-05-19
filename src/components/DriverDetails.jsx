c0f9286e53c3fd69e4e6ad07ef85b123786bfd62
Ksendza06 <ksendzavuu@gmail.com>
2026-05-19 14:20:25 +0200

    sredili smo tabelu u races

--- src/components/DriverDetails.jsx
+++ src/components/DriverDetails.jsx
@@ -5,8 +5,14 @@ import Loader from "./Loader";
 
 export default function DriverDetails() {
     const [driverDetails, setDriverDetails] = useState(null);
+    const [driverRaces, setDriverRaces] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+
     const params = useParams();
+
+    const handleClick = () => {
+        console.log("click");
+    }
 
     useEffect(() => {
         getDriverDetails();
@@ -14,43 +20,82 @@ export default function DriverDetails() {
 
     const getDriverDetails = async () => {
         console.log("params", params);
-        const url = `https://api.jolpi.ca/ergast/f1/2013/drivers/alonso/driverStandings.json${driverId}`;
-        // const url2 = ;
+        const urlDriverDetails = `https://api.jolpi.ca/ergast/f1/2013/drivers/${params.id}/driverStandings.json`;
+        const urlDriverRaces = `https://api.jolpi.ca/ergast/f1/2013/drivers/${params.id}/results.json`;
 
-        const response = await axios.get(url);
-        console.log("response", response);
-        console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
-        setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+        const response1 = await axios.get(urlDriverDetails);
+        const response2 = await axios.get(urlDriverRaces);
+        console.log("response1", response1);
+        console.log("response2", response2);
+        console.log(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+        console.log(response2.data.MRData.RaceTable.Races);
+        setDriverDetails(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+        setDriverRaces(response2.data.MRData.RaceTable.Races);
         setIsLoading(false);
         console.log("getDriversDetails");
+        console.log("getDriversRaces");
     };
 
     if (isLoading) {
         return <Loader />;
     };
+    console.log("driver " + driverDetails);
+    console.log("driverDetails ", driverDetails);
+    console.log("driverRaces ", driverRaces);
 
     return (
-        <>
-            <div>
-                <h2>Comment details</h2>
-                <p>Id: {driverDetails.driverId}</p>
-                <p>Id: {driverDetails.permanentNumber}</p>
-                <p>Id: {driverDetails.code}</p>
-                <p>Id: {driverDetails.givenName}</p>
-                <p>Id: {driverDetails.familyName}</p>
-                <p>Id: {driverDetails.dateOfBirth}</p>
-                <p>Id: {driverDetails.nationality}</p>
-                {/* <p>Name: {DriverDetails.name}</p>
-                        <p>Email: {DriverDetails.email}</p>
-                        <p>Body: {DriverDetails.body}</p> */}
-            </div>
-            <div>
+        <div className="container">
+            <div className="driver-details">
 
+                {driverDetails.map((driverDetail) => {
+                    return (
+                        <div key={driverDetail.position}>
+                            <img src="" alt="" />
+                            <p>Ime i prezime{driverDetail.Driver.givenName} {driverDetail.Driver.familyName}</p>
+                            <p>Country: {driverDetail.Driver.nationality}</p>
+                            <p>Team: {driverDetail.Constructors[0].name}</p>
+                            <p>Birth: {driverDetail.Driver.dateOfBirth}</p>
+                            <a href="#">Biography</a>
+                        </div>
+                    );
+                })}
             </div>
-            {/* <p>Id: {driverDetails.id}</p>
-            <p>Name: {driverDetails.name}</p>
-            <p>Email: {driverDetails.email}</p>
-            <p>Body: {driverDetails.body}</p> */}
-        </>
+
+            <div className="right-details">
+                <h3>Formula 1 2013 Results</h3>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Round</th>
+                            <th>Grand Prix</th>
+                            <th>Team</th>
+                            <th>Grid</th>
+                            <th>Race</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {driverRaces.map((driverRace) => {
+                            return (
+                                <tr onClick={() => handleClick(driver.Driver.driverId)}
+                                    key={driverRace.position}>
+
+                                    <td>{driverRace.round}</td>
+                                    <td>{driverRace.raceName}</td>
+                                    <td>{driverRace.Results[0].Constructor.name}</td>
+                                    <td>{driverRace.Results[0].grid}</td>
+                                    <td>{driverRace.Results[0].position}</td>
+
+                                </tr>
+                            );
+                        })}
+                        <tr>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     );
 }
