82f55ad652ece3d915d3522e0a2bf689c2ea73e0
Ksendza06 <ksendzavuu@gmail.com>
2026-05-19 15:51:39 +0200

    pocela racedetails

--- src/components/RaceDetails.jsx
+++ src/components/RaceDetails.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 export default function RaceDetails() {
+    const [raceDetais, setRaceDetails] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const params = useParams();
+
+    const handleClick = () => {
+        console.log("click");
+    }
+
+    useEffect(() => {
+        getRaceDetails();
+    }, [])
+
+    const getRaceDetails = async () => {
+        console.log("params", params);
+
+        // ====== > 3 url zavrsiti
+        // const urlResult = "";
+        // const urlQualifiers = "";
+        // const urlAllRaces = "";
+
+        const urlFlag = "https://raw.githubusercontent.com/Imagin-io/country-nationality-list/refs/heads/master/countries.json";
+
+        const response = await axios.get(urlDriverDetails);
+
+        console.log("response", response);
+
+        console.log(response1.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+
+        setDriverDetails();
+        setIsLoading(false);
+        console.log("getDriversRaces");
+    };
+
+    if (isLoading) {
+        return <Loader />;
+    };
+
+    console.log("raceDetails ", RaceDetails);
+
     return (
-        <>Race details</>
+        <div className="container">
+
+            <div className="">
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
     );
 }
