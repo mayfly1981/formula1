a0c94667144b3d2489f1ecfedfe666278acc9a78
fanuchi <fanuchi@gmail.com>
2026-05-19 12:50:15 +0200

    id

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -5,7 +5,8 @@ import Loader from "./Loader";
 
 
 export default function TeamDetails() {
-    const { teamId } = useParams();
+    const { id } = useParams();
+    const teamId = id;
     const year = 2013;
 
     const [races, setRaces] = useState([]);
