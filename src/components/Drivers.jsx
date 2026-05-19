cc20cf04ac17b8967aa0e6f9fad9fc3b57aea898
Ksendza06 <ksendzavuu@gmail.com>
2026-05-19 11:18:58 +0200

    sredila sam tabelu u Drivers-u

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -4,62 +4,61 @@ import Loader from "./Loader";
 import { useNavigate, } from "react-router";
 
 export default function Drivers() {
+
     const [drivers, setDrivers] = useState([])
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
-
         console.log("useEffect");
         getDrivers();
-
     }, []);
 
     const getDrivers = async () => {
-
         const url = "https://api.jolpi.ca/ergast/f1/2013/driverStandings.json";
         const response = await axios.get(url);
         console.log(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
         setDrivers(response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings);
         setLoading(false);
         console.log("getDrivers");
-
-
-
     };
+
     const handleClick = (driverId) => {
         console.log("handleClick", driverId);
         navigate(`/driverDetails/${driverId}`);
     };
+
     if (loading) {
         return <Loader />
-
     }
-    return (
-        <>
-            {drivers.map((driver) => {
-                return (
-                    <table key={driver.Driver.driverId}>
-                        <thead>
-                            <tr>
-                                <th>Drivers Championship Standings - 2013</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{driver.position}</td>
-                                <td
-                                    onClick={() => handleClick(driver.Driver.driverId)}
-                                    className="driver-details" key={driver.Driver.driverId}
-                                >{driver.Driver.givenName}{driver.Driver.familyName}</td>
-                                <td>{driver.Constructors[0].name}</td>
-                                <td>{driver.points}</td>
-                            </tr>
-                        </tbody>
 
-                    </table>
-                )
-            })}
-        </>
+    return (
+        <div>
+            <h2 className="title">Drivers Championship Standings - 2013</h2>
+            <div>
+                <table style={{ width: "80%", tableLayout: "fixed" }}>
+                    <thead>
+                        <tr>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {drivers.map((driver) => {
+                            return (
+                                <tr onClick={() => handleClick(driver.Driver.driverId)}
+                                    className="driver-details"
+                                    key={driver.Driver.driverId}>
+                                    <td>{driver.position}</td>
+                                    <td
+                                    >{driver.Driver.givenName}{driver.Driver.familyName}</td>
+                                    <td>{driver.Constructors[0].name}</td>
+                                    <td>{driver.points}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
