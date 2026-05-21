3d8fe79754d6878b32de5d536cd90eada69ac3f3
mayfly1981 <mayfly1981@gmail.com>
2026-05-21 15:29:52 +0200

    nova komponenta

---
+++ src/components/Breadcrumb.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router"
+
+export default function Breadcrumb(props) {
+    return (
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                {props.items.map((item, i) => {
+                    const isLast = i === props.items.length - 1;
+                    return (
+                        <li>
+                            {!isLast ?
+                                <Link to={item.route}>{item.text}</Link>
+                                : <span>{item.text}</span>
+                            }
+                        </li>
+                    );
+                })}
+            </ul>
+        </div >
+    );
+}
