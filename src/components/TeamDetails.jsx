d48325b2b1ceedd2c2234829249bc7747501ff56
fanuchi <fanuchi@gmail.com>
2026-05-22 10:37:30 +0200

    klase

--- src/components/TeamDetails.jsx
+++ src/components/TeamDetails.jsx
@@ -119,7 +119,7 @@ export default function TeamDetails(props) {
 
             <h2>Formula 1 2013 Results</h2>
 
-            <table>
+            <table className="results-table">
                 <thead>
                     <tr>
                         <th>Round</th>
