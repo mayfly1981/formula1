b2d84769ba46ec22f09c35bc75683574a7cd2051
mayfly1981 <mayfly1981@gmail.com>
2026-05-25 13:02:49 +0200

    app izmena

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ import RaceDetails from "./components/RaceDetails";
 import Loader from "./components/Loader";
 import axios from "axios";
 import Home from "./components/Home";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function App() {
   const [flags, setFlags] = useState([]);
