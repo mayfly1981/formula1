e3258b62550545a8fc01b451d44d6707c72aa043
mayfly1981 <mayfly1981@gmail.com>
2026-05-26 14:54:30 +0200

    DriversDetails izmena onClick

--- src/components/Drivers.jsx
+++ src/components/Drivers.jsx
@@ -32,12 +32,12 @@ export default function Drivers(props) {
     const filteredDrivers = drivers.filter((driver) => `${driver.Driver.givenName} ${driver.Driver.familyName}`.toLowerCase().includes(search.toLowerCase().trim().replace(/\s+/g, " ")))
 
 
-
-
-
     const handleClick = (driverId) => {
-        console.log("handleClick", driverId);
         navigate(`/driverDetails/${driverId}`);
+    };
+
+    const handleClickTeam = (driverId) => {
+        navigate(`/teamDetails/${driverId}`);
     };
 
     if (loading) {
@@ -77,17 +77,17 @@ export default function Drivers(props) {
                     <tbody>
                         {filteredDrivers.map((driver) => {
                             return (
-                                <tr onClick={() => handleClick(driver.Driver.driverId)}
+                                <tr
                                     className="driver-details"
                                     key={driver.Driver.driverId}>
 
                                     <td>{driver.position}</td>
 
-                                    <td onClick={() => handleClick(driver.Driver.driverId)}>
+                                    <td >
                                         <Flag country={getCountryCodeByNationality(props.flags, driver.Driver.nationality)} size={20} />
                                     </td>
-                                    <td>{driver.Driver.givenName} {driver.Driver.familyName}</td>
-                                    <td>{driver.Constructors[0].name}</td>
+                                    <td onClick={() => handleClick(driver.Driver.driverId)}>{driver.Driver.givenName} {driver.Driver.familyName}</td>
+                                    <td onClick={() => handleClickTeam(driver.Constructors[0].constructorId)}>{driver.Constructors[0].name}</td>
                                     <td>{driver.points}</td>
                                 </tr>
                             );
