9a2a4001ec954a780fd8ae00405ce07b7f6faba8
Ksendza06 <ksendzavuu@gmail.com>
2026-05-26 15:17:17 +0200

    year - teamDetails

--- src/components/SelectYear.jsx
+++ src/components/SelectYear.jsx
@@ -7,7 +7,7 @@ export default function SelectYear(props) {
     //     years.push(year);
     // };
 
-    for (let i = 2026; i >= 2006; i--) {
+    for (let i = 2026; i >= 2010; i--) {
         years.push(i);
     }
 
