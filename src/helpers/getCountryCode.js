b93669087e1fac3557d44f1b927ea4e795f4b83c
MomciloSh <momcilosaran@gmail.com>
2026-05-26 15:35:33 +0200

    RacingDetails ispravke

--- src/helpers/getCountryCode.js
+++ src/helpers/getCountryCode.js
@@ -7,15 +7,15 @@ export function getCountryCodeByNationality(flags, nationality) {
         return "NL";
     }
 
-    console.log("flags ", flags, nationality)
+    // console.log("flags ", flags, nationality)
     const flag = flags.find(flag => flag.nationality === nationality);
-    console.log(flag);
+    // console.log(flag);
     return flag?.alpha_2_code;
 }
 
 export function getCountryCodeByShortName(flags, nationality) {
-    console.log("flags ", flags, nationality)
+    // console.log("flags ", flags, nationality)
     const flag = flags.find(flag => flag.en_short_name === nationality);
-    console.log(flag);
+    // console.log(flag);
     return flag?.alpha_2_code;
 }
