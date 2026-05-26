d47715b639aae9cba9de59cd7b2e7b60e6f3ae5c
fanuchi <fanuchi@gmail.com>
2026-05-26 13:12:37 +0200

    podeseno 10+1 boja i uslovi

--- src/helpers/positionColors.js
+++ src/helpers/positionColors.js
@@ -1,12 +1,25 @@
-export default function getPositionColor(position) {
+export default function getPositionColor(position, limit = 5) {
 
     const colors = {
-        1: "yellow",
-        2: "gray",
-        3: "orange",
-        4: "lightgreen",
-        5: "lightblue"
+        1: "#f2c319",
+        2: "#88be6a",
+        3: "#9a9999",
+        4: "#e98b1a",
+        5: "#6697ec",
+        6: "#10a9b4",
+        7: "#9b5de5",
+        8: "#f15bb5",
+        9: "#ef476f",
+        10: "#3a0ca3"
     };
 
-    return colors[position] || "#7393b3";
+    const pos = Number(position);
+
+    if (!position || Number.isNaN(pos)) {
+        return "#66696d";
+    }
+    if (pos > limit) {
+        return "#66696d";
+    }
+    return colors[position] || "#66696d";
 }
