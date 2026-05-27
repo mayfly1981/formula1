89f69b2daf2acccca4dae408fac8947fc3b37a36
Ksendza06 <ksendzavuu@gmail.com>
2026-05-27 14:20:33 +0200

    ispravak try-catch RACES

--- src/components/Races.jsx
+++ src/components/Races.jsx
@@ -27,12 +27,42 @@ export default function Races(props) {
             setError(false);
             const url = `https://api.jolpi.ca/ergast/f1/${props.year}/results/1.json`;
             const response = await axios.get(url);
-            const standings = response.data.MRData.StandingsTable.StandingsLists[0]
-                ?.DriverStandings || [];
+            const standings = response.data.MRData.RaceTable.Races || [];
             if (standings.length === 0) {
                 setError(true);
             } else {
                 setRaces(standings);
+            }
+        } catch (e) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // const getRaces = async () => {
+    //     const url = `https://api.jolpi.ca/ergast/f1/${props.year}/results/1.json`;
+    //     const response = await axios.get(url);
+    //     console.log(response.data.MRData.RaceTable.Races);
+    //     setRaces(response.data.MRData.RaceTable.Races);
+    //     setLoading(false);
+    //     console.log("getRaces");
+    // };
+
+
+
+    const getDrivers = async () => {
+        try {
+            setError(false);
+            const url = `https://api.jolpi.ca/ergast/f1/${props.year}/driverStandings.json`;
+            const response = await axios.get(url);
+            const standings =
+                response.data.MRData.StandingsTable.StandingsLists[0]
+                    ?.DriverStandings || [];
+            if (standings.length === 0) {
+                setError(true);
+            } else {
+                setDrivers(standings);
             }
         } catch (e) {
             setError(true);
