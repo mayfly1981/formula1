00f43c52f9b5254358dd4a86d317e1513150e7e1
mayfly1981 <mayfly1981@gmail.com>
2026-05-27 12:50:52 +0200

    Dodat AZ

--- src/helpers/getCountryCode.js
+++ src/helpers/getCountryCode.js
@@ -11,15 +11,14 @@ export function getCountryCodeByNationality(flags, nationality) {
         return "AZ";
     }
 
-    // console.log("flags ", flags, nationality)
     const flag = flags.find(flag => flag.nationality === nationality);
     // console.log(flag);
     return flag?.alpha_2_code;
 }
 
 export function getCountryCodeByShortName(flags, nationality) {
-    // console.log("flags ", flags, nationality)
+
     const flag = flags.find(flag => flag.en_short_name === nationality);
-    // console.log(flag);
+
     return flag?.alpha_2_code;
 }
