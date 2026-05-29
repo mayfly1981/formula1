bdb9336c5ffdaa093793d4b8b9d03eafd88dbb7a
fanuchi <fanuchi@gmail.com>
2026-05-29 08:46:17 +0200

    menjano da bude lep

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router";
 import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import SelectYear from "./SelectYear";
 
-export default function Breadcrumb(props) {
+export default function Breadcrumb({ items, search, onSearch, placeholder = "Search...", year, onYearChange }) {
     return (
         <nav className="breadcrumbs-wrapper">
             <ul className="breadcrumbs">
@@ -11,21 +13,37 @@ export default function Breadcrumb(props) {
                         <span>Home</span>
                     </Link>
                 </li>
-                {props.items.map((item, i) => {
-                    const isLast = i === props.items.length - 1;
+
+                {items.map((item, i) => {
+                    const isLast = i === items.length - 1;
                     return (
                         <li className="breadcrumb-item" key={item.text}>
                             {!isLast ? (
-                                <Link to={item.route}>
-                                    {item.text}</Link>
+                                <Link to={item.route}>{item.text}</Link>
                             ) : (
-                                <span key={i}>{item.text}
-                                </span>
+                                <span className="breadcrumb-active">{item.text}</span>
                             )}
                         </li>
                     );
                 })}
+
+                <li className="breadcrumbs-controls">
+                    {onYearChange && (
+                        <SelectYear value={year} change={(e) => onYearChange(e.target.value)} />
+                    )}
+                    {onSearch && (
+                        <div className="breadcrumb-search">
+                            <SearchIcon fontSize="small" />
+                            <input
+                                type="text"
+                                placeholder={placeholder}
+                                value={search}
+                                onChange={(e) => onSearch(e.target.value)}
+                            />
+                        </div>
+                    )}
+                </li>
             </ul>
         </nav>
     );
-};
+}
